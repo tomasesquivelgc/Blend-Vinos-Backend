1cd4d69c0bcfaeeb16cb4302b375d73299cf8575
Tomas Esquivel <tomasesquivel@yahoo.com>
2026-01-27 18:20:12 -0300

    fix typo

--- src/models/historyDetailModel.js
+++ src/models/historyDetailModel.js
@@ -2,18 +2,18 @@ import db from "../db.js";
 
 export const addHistoryDetail = async ({
   id,
-  movimient_id,
+  movimiento_id,
   vino_id,
   cantidad,
   precio_unitario
 }) => {
   const query = `
     INSERT INTO historial_detalle
-    (id, movimient_id, vino_id, cantidad, precio_unitario)
+    (id, movimiento_id, vino_id, cantidad, precio_unitario)
     VALUES ($1, $2, $3, $4, $5)
     RETURNING *;
   `;
-  const values = [id, movimient_id, vino_id, cantidad, precio_unitario];
+  const values = [id, movimiento_id, vino_id, cantidad, precio_unitario];
   const { rows } = await db.query(query, values);
   return rows[0];
 };
